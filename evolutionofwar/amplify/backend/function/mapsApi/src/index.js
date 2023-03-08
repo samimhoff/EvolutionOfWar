49a9fb14332126ced620c0322da295f32304c99c
samimhoff <67920224+samimhoff@users.noreply.github.com>
2023-03-08 09:34:58 -0500

    Removed boiler plate code

--- evolutionofwar/amplify/backend/function/mapsApi/src/index.js
+++ evolutionofwar/amplify/backend/function/mapsApi/src/index.js
@@ -5,13 +5,15 @@
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    const mapsId = event.pathParameters.mapsId;
+    const maps = {'mapsid: ': mapsId};
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     }, 
+        body: JSON.stringify(maps),
     };
 };
--- evolutionofwar/src/App.tsx
+++ evolutionofwar/src/App.tsx
@@ -1,52 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import Amplify, { API } from 'aws-amplify'
-import React, { useEffect, useState } from 'react'
+import { API } from 'aws-amplify'
+import { useEffect, useState } from 'react'
 
 const myAPI = "evolutionapi"
-const path = '/entries'; 
+const path = '/maps'; 
 
 const App = () => {
-  const [input, setInput] = useState("")
-  const [customers, setCustomers] = useState([])
+  const [mapsKey, setMapsKey] = useState('');
 
-  //Function to fetch from our backend and update customers array
-  function getCustomer(e: any) {
-    let entryId = e.input
-    //@ts-ignore
-    API.get(myAPI, path + "/" + entryId)
-       .then(response => {
-         console.log(response)
-         let newCustomers = [...customers]
-        //@ts-ignore
-         newCustomers.push(response)
-         setCustomers(newCustomers)
+  //Pretending to get mapsAPIKey
+  useEffect(() => {
+  //@ts-ignore
+    API.get(myAPI, path + "/" + 1)
+    .then(response => {
+      console.log(response)
+      setMapsKey(response);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, []);
 
-       })
-       .catch(error => {
-         console.log(error)
-       })
-  }
 
   return (
     
     <div className="App">
-      <h1>Super Simple React App</h1>
-      <div>
-          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
-      </div>
-      <br/>
-      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
-
-      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
-      {
-       customers.map((thisCustomer: any, index) => {
-         return (
-        <div key={thisCustomer.entryId}>
-          <span><b>CustomerId:</b> {thisCustomer.entryId} - <b>CustomerName</b>: {thisCustomer.entryName}</span>
-        </div>)
-       })
-      }
+      
     </div>
   )
 }
